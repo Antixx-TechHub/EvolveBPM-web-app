1192e6d7c2cda7310d0d4cd03007ef899dd51d7e
AntixxTechHub <info@antixxtechhub.com>
2023-09-20 15:55:08 +0530

    final evolve code

---
+++ components/Index/MainBanner.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="banner-area tpbanner-space-two" data-background="assets/img/banner/banner-2-bg-1.png">
+         <div class="container">
+            <div class="row justify-content-center">
+               <div class="col-lg-7 col-md-7 order-2 order-md-1">
+                  <div class="tpbanner-content-two">
+                     <div class="tpbanner-title-two mb-20"><span>Search Engine
+                           <svg width="406" height="40" viewBox="0 0 406 40" fill="none"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path d="M3.14977 24.4029C3.14977 24.4029 256.329 -7.75032 398.26 15.5002"
+                                 stroke="#FFCE5A" stroke-width="5" stroke-linecap="round" />
+                              <path d="M3.59029 19.0626C3.59029 19.0626 261.471 -1.12129 402.878 28.422"
+                                 stroke="#FFCE5A" stroke-width="5" stroke-linecap="round" />
+                           </svg>
+                        </span> Optimization <br> Made Easy.</div>
+                     <p>SEOMY makes it simple to design a good website <br> take your idea online!</p>
+                     <div class="tpbanner-two-btn mb-35">
+                        <a class="green-btn" href="contact.html">Start Ranking Now</a>
+                     </div>
+                     <div class="tpbanner-two-rating">
+                        <span>Excellent</span>
+                        <img src="../../assets/img/shape/rating-1.png" alt="">
+                        <p>Trustpilot</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-5 col-md-5 order-1 order-md-2">
+                  <div class="tpbanner-two-shape p-relative" data-parallax='{"x": 15, "smoothness": 10}'>
+                    <img src="../../assets/img/banner/banner-2-thumb-1.png" alt="image">
+                     <div class="tpbanner-two-shape-round">
+                        <div class="tpbanner-two-shape-one">
+
+                           <img src="../../assets/img/banner/banner-2-shape-1.png" alt="">
+                        </div>
+                        <div class="tpbanner-two-shape-two">
+                           <img src="../../assets/img/banner/banner-2-shape-2.png" alt="">
+                        </div>
+                     </div>
+                     <div class="tpbanner-two-shape-three">
+                        <img src="../../assets/img/banner/banner-2-shape-3.png" alt="">
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MainBanner',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <!-- <NavbarStyleTwo /> -->
+      <MainBanner />
+
+      <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+  // import NavbarStyleTwo from '../layouts/Navbar'
+  import MainBanner from '../components/Index/MainBanner'
+
+  // import Footer from '../layouts/Footer'
+
+  export default {
+      components: {
+          // NavbarStyleTwo,
+          MainBanner,
+
+          // Footer,
+      }
+  }
+</script>
