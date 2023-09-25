46e7aa2381ebd0eee3ad82f3667406b98ea0055b
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-09-25 17:02:31 +0530

    team API done

--- components/AboutUs/TeamAreaHomeFive.vue
+++ components/AboutUs/TeamAreaHomeFive.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="team-area pb-50">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-3 text-center mb-35">
-                        <div class="section-3-title mb-10">Meet our team of <br> expert</div>
+        <div class="container " >
+            <div class="row ">
+                <div class="col-lg-12 pb-50">
+                    <div class="section-3 text-center mb-35 ">
+                        <div class="section-3-title mb-10 ">Meet our team of <br> expert</div>
                         <p>We're a 100% remote team spread all across the world!</p>
                     </div>
                     <div class="team-inner-all  text-center mb-70">
@@ -12,25 +12,27 @@
                     </div>
                 </div>
             </div>
-            <div class="row gx-9 pb-30">
-                <div class="col-lg-3 col-md-6">
-                    <div class="team-5-item text-center mb-40">
-                        <div class="team-5-thumb mb-25">
-                            <img src="../../assets/img/team/team-5-1.png" alt="">
+            <div class="row gx-9 pb-30" v-if="teams !== null">
+                <div class="col-lg-3 col-md-6" v-for="team in teams.singleTeam" :key="team.id" >
+                    <div class="team-5-item text-center mb-40" >
+                        <div class="team-5-thumb mb-25" >
+                            <img :src="team.image.data.attributes.url" alt="teams">
                             <div class="team-5-social">
                                 <span class="icon"></span>
                                 <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
                                 <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                <a :href="team.linkedin"
+                                    class="d-block"
+                                    target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
                             </div>
                         </div>
                         <div class="team-5-content">
-                            <h4 class="team-5-title"><a href="team-details.html">Danny Russell</a></h4>
-                            <p>Founder & CEO</p>
+                            <h4 class="team-5-title"><a href="team-details.html">{{ team.title }}</a></h4>
+                            <p>{{ team.designation }}</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <!-- <div class="col-lg-3 col-md-6">
                     <div class="team-5-item team-5-item-2 text-center mb-40">
                         <div class="team-5-thumb mb-25">
                             <img src="../../assets/img/team/team-5-2.png" alt="">
@@ -46,8 +48,8 @@
                             <p>Founder & CEO</p>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
+                </div> -->
+                <!-- <div class="col-lg-3 col-md-6">
                     <div class="team-5-item team-5-item-3 text-center mb-40">
                         <div class="team-5-thumb mb-25">
                             <img src="../../assets/img/team/team-5-3.png" alt="">
@@ -80,15 +82,26 @@
                             <p>Founder & CEO</p>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
 </template>
  
 <script>
+import axios from 'axios'
 
 export default {
     name: 'TeamAreaHomeFive',
+    data() {
+        return {
+            teams: null,
+        }
+    },
+    created: async function () {
+        const response = await axios.get('https://evolvestrapi.pbwebvision.in/api/team?populate=deep,5')
+        const { data: { attributes } } = response.data
+        this.teams = attributes
+    },
 }
 </script>
---
+++ pages/our-team.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Navbar />
+        <TeamAreaHomeFive />
+        <Partner />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from '../layouts/Navbar'
+import TeamAreaHomeFive from '../components/AboutUs/TeamAreaHomeFive'
+import Partner from '../components/Index/Partner'
+import Footer from '../layouts/Footer'
+
+export default {
+    components: {
+        Navbar,
+        TeamAreaHomeFive,
+        Partner,
+        Footer,
+    },
+};
+</script>
