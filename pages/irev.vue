75829c6623576cadb10bff34a4fa3a2b21617822
AntixxTechHub <info@antixxtechhub.com>
2023-09-27 12:48:47 +0530

    irev

---
+++ pages/irev.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from '../layouts/Navbar'
+
+  import Footer from '../layouts/Footer'
+  
+  export default {
+    components: {
+      Navbar,
+
+      Footer,
+    }
+  }
+  </script>
