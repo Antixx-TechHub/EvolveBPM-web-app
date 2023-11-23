ffef657157564e7e04a98174f13f6346d492bf78
AntixxTechHub <info@antixxtechhub.com>
2023-11-23 16:21:25 +0530

    new

---
+++ components/SuccessStories/SuccessStoryMain.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="postbox__area pt-120 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6" v-for="blog in blogs.slice(
+                    (currentPage - 1) * perPage,
+                    currentPage * perPage,
+                )" :key="blog.id">
+                    <div class="single-blog-post">
+                        <div class="post-image">
+                            <NuxtLink :to="'/success-stories-details/' + blog.attributes.slug" class="d-block">
+                                <img :src="blog.attributes.image.data.attributes.url" alt="blog">
+                            </NuxtLink>
+                        </div>
+
+                        <div class="post-content">
+                            <div class="post-meta">
+                                <ul>
+                                    <li>By: {{ blog.attributes.author }}</li>
+                                    <li>{{ blog.attributes.date }}</li>
+                                </ul>
+                            </div>
+
+                            <h3>
+                                <NuxtLink :to="'/success-stories-details/' + blog.attributes.slug">
+                                    {{ blog.attributes.title }}
+                                </NuxtLink>
+                            </h3>
+                            
+                            <p>{{ blog.attributes.shortDesc }}</p>
+
+                            <NuxtLink :to="'/success-stories-details/' + blog.attributes.slug" class="read-more-btn">
+                                Read More
+                                <i class="flaticon-right-arrow"></i>
+                            </NuxtLink>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <b-pagination class="basic-pagination mt-30" v-model="currentPage" :total-rows="rows" :per-page="perPage"
+                    aria-controls="itemList" align="center">
+                </b-pagination>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'Blog',
+    data() {
+        return {
+            blogs: [],
+            rows: 0,
+            currentPage: 1,
+            perPage: 6,
+        }
+    },
+    created: async function () {
+        const response = await axios.get('https://evolvestrapi.pbwebvision.in/api/successstories?populate=*')
+        this.blogs = response.data.data;
+        this.rows = this.blogs?.length;
+    },
+}
+</script>
--- pages/success-stories-details/_slug/index.vue
+++ pages/success-stories-details/_slug/index.vue
@@ -1,49 +1,9 @@
 <template>
     <div>
-        <NavbarTwo />
-        <div class="blog-details-area blog-details-bg pb-120 pt-200" :style="bannerStyle">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="blog-details-content" v-if="details !== null">
-                            <div class="blog-details-tag">
-                                <span><a href="blog-details.html">{{ details[0].attributes.shortDesc }}</a></span>
-                            </div>
-                            <h4 class="blog-details-banner-title">{{ details[0].attributes.title }}</h4>
-                            <div class="blog-details-meta">
-                                <span class="blog-details-meta-author">
-                                    <a href="#">
-                                        <img :src="details[0].attributes.avtar.data.attributes.url" alt="blog">
-                                        {{ details[0].attributes.author }}
-                                    </a>
-                                </span>
-                                <span>
-                                    <i>
-                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M15 8C15 11.864 11.864 15 8 15C4.136 15 1 11.864 1 8C1 4.136 4.136 1 8 1C11.864 1 15 4.136 15 8Z"
-                                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                                stroke-linejoin="round" />
-                                            <path
-                                                d="M10.5967 10.226L8.42672 8.93099C8.04872 8.70699 7.74072 8.16799 7.74072 7.72699V4.85699"
-                                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                                stroke-linejoin="round" />
-                                        </svg>
-                                    </i>
-                                    {{ details[0].attributes.date }}
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="inner-shape-dots">
-                <img src="../../../assets/img/shape/inner-dots-shape.png" alt="">
-            </div>
-        </div>
+        <Navbar />
+        <PageTitle v-if="details !== null" :pageTitle="details[0].attributes.title" pageDesc="News and Insights" />
         <div v-if="details !== null">
-            <BlogDetails v-bind:detailsContent="details" />
+            <SuccesStoryDetails v-bind:detailsContent="details" />
         </div>
         <Partner />
         <Footer />
@@ -51,18 +11,18 @@
 </template>
 
 <script>
-import NavbarTwo from '../../../layouts/NavbarTwo';
-import BlogDetails from '../../../components/SuccessStories/SuccesStoryDetails'
-import PageTitle from '../../../components/Common/PageTitle';
+import Navbar from '../../../layouts/Navbar'
+import PageTitle from '../../../components/Common/PageTitle'
+import SuccesStoryDetails from '../../../components/SuccessStories/SuccesStoryDetails'
 import Partner from '../../../components/Common/Partner'
 import Footer from '../../../layouts/Footer'
 import axios from 'axios';
 
 export default {
     components: {
-        NavbarTwo,
+        Navbar,
         PageTitle,
-        BlogDetails,
+        SuccesStoryDetails,
         Partner,
         Footer,
     },
@@ -84,9 +44,7 @@ export default {
         const pageData = this.details.length > 0 ? this.details[0] : {};
         if (pageData?.attributes?.seo) {
             this.seoData = pageData.attributes.seo;
-            // console.log( this.seoData ,' this.seoData ');
         }
-        // console.log("this.details....", this.details);
     },
     head({ $seo }) {
 
@@ -94,7 +52,6 @@ export default {
             title: this.seoData?.metaTitle,
             description: this.seoData?.metaDescription,
             keywords: this.seoData?.keywords,
-            // image: this.post.image || '',
         });
     },
 };
