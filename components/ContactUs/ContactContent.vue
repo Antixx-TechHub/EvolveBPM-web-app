88b3eb4fec38b74089e887b3ef1e9cee89d5150e
AntixxTechHub <info@antixxtechhub.com>
2023-11-27 14:12:16 +0530

    Stat update

--- components/ContactUs/ContactContent.vue
+++ components/ContactUs/ContactContent.vue
@@ -51,69 +51,62 @@
                     <div class="overview-content">
                         <div class="content pb-40">
                             <h2>We are the next generation of the advertising world</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>                  
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>           
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-4 col-md-6 col-sm-6">
-                                <div class="contact-info-box">
-                                    <div class="icon">
-                                        <i class="flaticon-email"></i>
+
+                        <div class="fun-facts-two pt-100 pb-70 bg-f2f2f7">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-4 col-sm-6">
+                                        <div class="fun-fact-card">
+                                            <i class='bx bx-list-check'></i>
+                                            <h3>524<span class="sign-icon">+</span></h3>
+                                            <p>Completed Projects</p>
+                                        </div>
                                     </div>
 
-                                    <h3>Email Here</h3>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4 col-md-6 col-sm-6">
-                                <div class="contact-info-box">
-                                    <div class="icon">
-                                        <i class="flaticon-phone-call"></i>
+                                    <div class="col-lg-4 col-sm-6">
+                                        <div class="fun-fact-card">
+                                            <i class='bx bx-smile'></i>
+                                            <h3>560<span class="sign-icon">+</span></h3>
+                                            <p>Happy Clients</p>
+                                        </div>
                                     </div>
 
-                                    <h3>Location Here</h3>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
-                                <div class="contact-info-box">
-                                    <div class="icon">
-                                        <i class="flaticon-marker"></i>
+                                    <div class="col-lg-4 col-sm-6">
+                                        <div class="fun-fact-card">
+                                            <i class='bx bx-user-plus'></i>
+                                            <h3>128<span class="sign-icon">+</span></h3>
+                                            <p>Expert Members</p>
+                                        </div>
                                     </div>
 
-                                    <h3>Call Here</h3>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="row pt-40">
-                            <div class="col-lg-4 col-md-6 col-sm-6">
-                                <div class="contact-info-box">
-                                    <div class="icon">
-                                        <i class="flaticon-email"></i>
+                                    <div class="col-lg-4 col-sm-6">
+                                        <div class="fun-fact-card">
+                                            <i class='bx bx-trophy'></i>
+                                            <h3>29<span class="sign-icon">+</span></h3>
+                                            <p>Winning Awards</p>
+                                        </div>
                                     </div>
 
-                                    <h3>Email Here</h3>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4 col-md-6 col-sm-6">
-                                <div class="contact-info-box">
-                                    <div class="icon">
-                                        <i class="flaticon-phone-call"></i>
+                                    <div class="col-lg-4 col-sm-6">
+                                        <div class="fun-fact-card">
+                                            <i class='bx bx-user-plus'></i>
+                                            <h3>128<span class="sign-icon">+</span></h3>
+                                            <p>Expert Members</p>
+                                        </div>
                                     </div>
 
-                                    <h3>Location Here</h3>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
-                                <div class="contact-info-box">
-                                    <div class="icon">
-                                        <i class="flaticon-marker"></i>
+                                    <div class="col-lg-4 col-sm-6">
+                                        <div class="fun-fact-card">
+                                            <i class='bx bx-trophy'></i>
+                                            <h3>29<span class="sign-icon">+</span></h3>
+                                            <p>Winning Awards</p>
+                                        </div>
                                     </div>
-
-                                    <h3>Call Here</h3>
                                 </div>
                             </div>
                         </div>
@@ -128,11 +121,11 @@
                                     <div class="form-group">
                                         <select type="text" name="name" id="name" class="form-control" required
                                             placeholder="Name">
-                                        <option value="" disabled selected>I am intered in learning more about:</option>
-                                        <option value="0 to 3 Years">Option 1</option>
-                                        <option value="4 to 6 Years">Option 2</option>
-                                        <option value="7 to 10 Years">Option 3</option>
-                                    </select>
+                                            <option value="" disabled selected>I am intered in learning more about:</option>
+                                            <option value="0 to 3 Years">Option 1</option>
+                                            <option value="4 to 6 Years">Option 2</option>
+                                            <option value="7 to 10 Years">Option 3</option>
+                                        </select>
                                     </div>
                                 </div>
 
@@ -180,11 +173,11 @@
 
                                 <div class="col-lg-12 col-md-12">
                                     <div class="form-group">
-                                        <input class="form-check-input" type="checkbox" value=""
-                                                        id="flexCheckDefault">
-                                                    <label class="form-check-label" for="flexCheckDefault">
-                                                        I agree to allow Evolve Global Corp. to process my personal data in compliance with its <router-link to="/privacy-policy">privacy policy.</router-link>
-                                                    </label>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            I agree to allow Evolve Global Corp. to process my personal data in compliance
+                                            with its <router-link to="/privacy-policy">privacy policy.</router-link>
+                                        </label>
                                     </div>
                                 </div>
 
