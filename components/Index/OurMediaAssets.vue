a2a2fd7a3d4d33b508323de5dc2ec419b80852d5
AntixxTechHub <info@antixxtechhub.com>
2023-12-04 11:05:01 +0530

    slider

--- components/Index/OurMediaAssets.vue
+++ components/Index/OurMediaAssets.vue
@@ -1,49 +1,176 @@
 <template>
-        <div class="it-banner ptb-100">
-            <div class="d-table">
-                <div class="d-table-cell">
-                    <div class="container">
-                        <div class="section-title">
-                            <h2>Our Media Assets</h2>
-                        </div>
-                        <div class="row align-items-center">
+    <div class="unique-feedback-area pt-100">
+        <div class="container">
+            <div class="section-title">
+                <h2>Our Media Assets</h2>
+            </div>
 
-                            <div class="col-5">
-                                <carousel :autoplay="true" :loop="true" :autoplayTimeout="7000" :speed="1000"
-                                    :paginationEnabled="false" :perPageCustom="[[0, 1]]"
-                                    class="it-banner-image owl-carousel owl-theme">
-                                    <slide>
-                                        <div class="image">
-                                            <img src="~/assets/images/it-banner/animate-img.jpg" alt="image">
-                                        </div>
-                                    </slide>
+            <div class="row">
+                <div class="col-5 unique-feedback-slides">
+                    <carousel :autoplay="true" :loop="true" :autoplayTimeout="7000" :speed="1000" :paginationEnabled="true"
+                        :perPageCustom="[[0, 1], [768, 1], [1200, 1], [1550, 1]]">
+                        <slide>
+                            <div class="unique-single-feedback">
+                                <ul class="rating">
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                </ul>
+                                <p>“We believe brand interaction is key in communication. Real innovations and a positive
+                                    customer experience are the heart of successful communication. No fake products and
+                                    services.”</p>
+                                <div class="client-info">
+                                    <img src="~/assets/images/cryptocurrency-home/user1.jpg" alt="image">
 
-                                    <slide>
-                                        <div class="image">
-                                            <img src="~/assets/images/it-banner/animate-img2.jpg" alt="image">
-                                        </div>
-                                    </slide>
-                                </carousel>
-                            </div>
-
-                            <div class="col-7">
-                                <div class="container">
-                                    <MicrositeLogos />
+                                    <h3>Jason Roy</h3>
+                                    <span>Manager</span>
+                                </div>
+                                <div class="quote">
+                                    <img src="~/assets/images/cryptocurrency-home/quote.png" alt="image">
                                 </div>
                             </div>
+                        </slide>
 
-                        </div>
+                        <slide>
+                            <div class="unique-single-feedback">
+                                <ul class="rating">
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                </ul>
+                                <p>“We believe brand interaction is key in communication. Real innovations and a positive
+                                    customer experience are the heart of successful communication. No fake products and
+                                    services.”</p>
+                                <div class="client-info">
+                                    <img src="~/assets/images/cryptocurrency-home/user2.jpg" alt="image">
+
+                                    <h3>James Anderson</h3>
+                                    <span>Web Developer</span>
+                                </div>
+                                <div class="quote">
+                                    <img src="~/assets/images/cryptocurrency-home/quote.png" alt="image">
+                                </div>
+                            </div>
+                        </slide>
+
+                        <slide>
+                            <div class="unique-single-feedback">
+                                <ul class="rating">
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                </ul>
+                                <p>“We believe brand interaction is key in communication. Real innovations and a positive
+                                    customer experience are the heart of successful communication. No fake products and
+                                    services.”</p>
+                                <div class="client-info">
+                                    <img src="~/assets/images/cryptocurrency-home/user3.jpg" alt="image">
+
+                                    <h3>Jason Roy</h3>
+                                    <span>Manager</span>
+                                </div>
+                                <div class="quote">
+                                    <img src="~/assets/images/cryptocurrency-home/quote.png" alt="image">
+                                </div>
+                            </div>
+                        </slide>
+
+                        <slide>
+                            <div class="unique-single-feedback">
+                                <ul class="rating">
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                </ul>
+                                <p>“We believe brand interaction is key in communication. Real innovations and a positive
+                                    customer experience are the heart of successful communication. No fake products and
+                                    services.”</p>
+                                <div class="client-info">
+                                    <img src="~/assets/images/cryptocurrency-home/user1.jpg" alt="image">
+
+                                    <h3>James Anderson</h3>
+                                    <span>Web Developer</span>
+                                </div>
+                                <div class="quote">
+                                    <img src="~/assets/images/cryptocurrency-home/quote.png" alt="image">
+                                </div>
+                            </div>
+                        </slide>
+
+                        <slide>
+                            <div class="unique-single-feedback">
+                                <ul class="rating">
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                </ul>
+                                <p>“We believe brand interaction is key in communication. Real innovations and a positive
+                                    customer experience are the heart of successful communication. No fake products and
+                                    services.”</p>
+                                <div class="client-info">
+                                    <img src="~/assets/images/cryptocurrency-home/user2.jpg" alt="image">
+
+                                    <h3>Jason Roy</h3>
+                                    <span>Manager</span>
+                                </div>
+                                <div class="quote">
+                                    <img src="~/assets/images/cryptocurrency-home/quote.png" alt="image">
+                                </div>
+                            </div>
+                        </slide>
+
+                        <slide>
+                            <div class="unique-single-feedback">
+                                <ul class="rating">
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                    <li><i class='bx bxs-star'></i></li>
+                                </ul>
+                                <p>“We believe brand interaction is key in communication. Real innovations and a positive
+                                    customer experience are the heart of successful communication. No fake products and
+                                    services.”</p>
+                                <div class="client-info">
+                                    <img src="~/assets/images/cryptocurrency-home/user3.jpg" alt="image">
+
+                                    <h3>James Anderson</h3>
+                                    <span>Web Developer</span>
+                                </div>
+                                <div class="quote">
+                                    <img src="~/assets/images/cryptocurrency-home/quote.png" alt="image">
+                                </div>
+                            </div>
+                        </slide>
+                    </carousel>
+                </div>
+
+                <div class="col-7">
+                    <div class="container">
+                        <MicrositeLogos />
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <script>
+
 import MicrositeLogos from '../Carousels/MicrositeLogos'
 
 export default {
-    name: 'MainBannerFour',
+    name: 'Feedback',
     components: {
         MicrositeLogos
     },
